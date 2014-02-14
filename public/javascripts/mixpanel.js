27e0b141cc0a5a5e706249e2521cea493824c51f
Andy Brown <brownie3003@gmail.com>
2014-02-14 13:00:44 +0000

    Mixpanel done

--- public/javascripts/mixpanel.js
+++ public/javascripts/mixpanel.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-	$("#home").click() {
+	mixpanel.track("Landing Page Loaded");
+
+	// Navbar links
+	$("a[href='#home']").click(function() {
 		mixpanel.track("Menu: Home");
-	}
-}
+	});
+
+	$("a[href='#how']").click(function() {
+		mixpanel.track("Menu: How it works");
+	});
+
+	$("a[href='#pricing']").click(function() {
+		mixpanel.track("Menu: Pricing");
+	});
+
+	$("a[href='#features']").click(function() {
+		mixpanel.track("Menu: Features");
+	});
+
+	$("a[href='#FAQ']").click(function() {
+		mixpanel.track("Menu: FAQ");
+	});
+
+	// Emails submission
+	$("button#saveForm").click(function() {
+		mixpanel.track("Email submission")
+	});
+
+});
